bbf6c56d5ea31f44ccb65be7e67a1688c4f703a0
Duane Edmonds <duane.edmonds@gmail.com>
2025-12-06 14:55:34 -0800

    added command alias

--- src/sonia/commands.py
+++ src/sonia/commands.py
@@ -168,7 +168,7 @@ def update_cmd_execute(args: tuple[str, ...]) -> None:
     cons.send_confirmation(confirmation_note, "updated")
 
 update_cmd = Command(
-    ('change', 'update', 'u', 'edit', 'e'),
+    ('change', 'update', 'upd', 'u', 'edit', 'e'),
     update_cmd_execute
 )
 
