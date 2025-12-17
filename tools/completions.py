b0ceba61ec6d7e18fa75bc5070fb1528035b2408
Duane Edmonds <duane.edmonds@gmail.com>
2025-12-17 00:26:11 -0800

    simplified code

--- tools/completions.py
+++ tools/completions.py
@@ -26,9 +26,9 @@ SUBCOMMANDS_AND_OPTIONS = (
 )
 
 
-def get_suggestions(stub: str) -> tuple[str, ...]:
+def get_suggestions(stub: str) -> str:
     """Use the word STUB to return a list of relevant suggestions."""
-    return tuple(cmd for cmd in SUBCOMMANDS_AND_OPTIONS if cmd.startswith(stub))
+    return "\n".join(cmd for cmd in SUBCOMMANDS_AND_OPTIONS if cmd.startswith(stub))
 
 
 if __name__ == "__main__":
@@ -38,8 +38,6 @@ if __name__ == "__main__":
     # 1.  Script: sonia_completions.py (The logic provider)
     # 2.  Command ($1 / argv[1]): sonia (The context/namespace)
     # 3.  Stub ($2 / argv[2]): sub (What you typed so far)
-
-    stub = sys.argv[2]
-
+    #
     # bash expects newline-separated completions sent to standard output (stdout)
-    print("\n".join(get_suggestions(stub)))
+    print(get_suggestions(sys.argv[2]))
